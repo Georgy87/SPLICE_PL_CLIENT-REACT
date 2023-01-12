1b9f905b524925e42aa3d1ec24019690c5ebe334
Georgy87 <goshana87@mail.ru>
2023-01-12 21:31:06 +0300

    tests/#4 tests for packs

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,6 @@ import { packsReducer } from './slices/pack/packSlice';
 import { samplesReducer } from './slices/samples/samplesSlice';
 import { userReducer } from './slices/user/userSlice';
 
-// export const store = configureStore({
-// 	reducer: {
-// 		packs: packSlice.reducer,
-// 		samples: samplesSlice.reducer,
-// 		user: userSlice.reducer,
-// 	},
-// 	middleware: [thunk],
-// });
-
-
 export const rootReducer = combineReducers({
 	packs: packsReducer,
 	samples: samplesReducer,
--- src/tests/pages/service.ts
+++
@@ -1,5 +0,0 @@
-export class MyClass {
-	public doSomethingElse(arg1: number, arg2: number) {
-		arg1 + arg2;
-	}
-}
