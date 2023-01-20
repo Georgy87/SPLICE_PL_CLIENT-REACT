bbcc825199b1e565e8acf4f2e909051ccbef546c
Георгий Петренко <58736243+Georgy87@users.noreply.github.com>
2023-01-20 22:52:46 +0300

    hotfix/updated-base-url

--- src/constans/env.ts
+++ src/constans/env.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8000';
-// export const BASE_URL = process.env.HEROKY_API_URL;
+// export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://sample-cloud-server.herokuapp.com';
